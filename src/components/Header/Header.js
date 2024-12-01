162b3a279863ed65a27af8df7a1ab43d9050f142
Foglia-e-Amore <taku.odawara@gmail.com>
2024-12-01 10:16:04 -0600

    Placeholder header contents

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,16 @@
 import React from "react";
+import './Header.css'
 
 function Header() {
     return(
         <div className="Header">
-            <h1>Header Header</h1>
+            <h1 className="title">Header Header</h1>
+            <div className="items">
+                <p>paragraph</p>
+                <p>2Paragraph</p>
+                <p>3 paragraph</p>
+            </div>
+
         </div>
     )
 }
