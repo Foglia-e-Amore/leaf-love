18709fcde9feab3458044c9a95da3843497c99d1
nikhi <nikhilbob2009@gmail.com>
2024-12-28 16:39:35 -0600

    moo moo

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -1,11 +1,15 @@
 import React from "react";
 import './Homepage.css'
+import cows from "../../images/cows.png";
 
 function Homepage() {
     return(
-        <div className="Homepage">
-            <h1>Homepage</h1>
+    <div className="Homepage">
+        <div className="firststuff"> 
+            <img className = "mainimage" src={cows} alt = "cow ran away"/>
+            <p1 className = "imgtext">moo moo lil nigga</p1>
         </div>
+    </div>
     )
 }
 
